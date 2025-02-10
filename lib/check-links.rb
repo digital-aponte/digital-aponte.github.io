afdf1693f5f47788bf77b67d0def823e6d97c77e
mnyrop <marii@nyu.edu>
2025-02-10 14:36:41 -0500

    bulk remediate

--- lib/check-links.rb
+++ lib/check-links.rb
@@ -2,7 +2,11 @@ require 'html-proofer'
 
 opts = {
   checks: ['Links', 'Scripts', 'Images'],
+  ignore_urls: [/www.miamiartguide.com/],
   ignore_empty_alt: true,
+  allow_missing_href: true,
+  check_internal_hash: false,
+  check_external_hash: false,
   ignore_missing_alt: true,
   enforce_https: true
 }
